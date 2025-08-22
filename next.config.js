2adcdbd863f4d57fa91a5f3c5e1d68691a735472
itbedas <it.bedas@gmail.com>
2025-08-22 17:54:05 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,13 @@
-module.exports = { reactStrictMode: true };
+// next.config.js
+const path = require('path');
+
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    // "@/..." artık proje köküne işaret eder
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
+
+module.exports = nextConfig;
